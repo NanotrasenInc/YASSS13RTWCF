24e7f90b85c997426839b0e3625c24cae03167b0
clusterfack <afleck1@alumni.nd.edu>
2016-11-07 20:41:51 -0600

    Okay this lets you render shit

--- client/src/main.rs
+++ client/src/main.rs
@@ -10,27 +10,33 @@ use piston::window::WindowSettings;
 use piston::event_loop::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL, Texture};
-use graphics::{Image, clear, draw_state, DrawState};
-use graphics::rectangle::square;
+use graphics::{Image, clear, draw_state};
 use std::path::Path;
 use piston::input::RenderEvent;
 
 fn main() {
-    let opengl  = OpenGL::V3_2;
-    let mut gl  = GlGraphics::new(opengl);
+    test::test();
 
-    let mut window: GlutinWindow = WindowSettings::new("Useless Window!", [640, 640])
+    create_window();
+}
+
+fn create_window() {
+    let opengl  = OpenGL::V3_2;
+
+    let mut window: Window = WindowSettings::new("Useless Window!", [640, 640])
         .opengl(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
 
+    let mut gl  = GlGraphics::new(opengl);
+
     //Create the image object and attach a square Rectangle object inside.
-    let image   = Image::new().rect(square(0.0, 0.0, 200.0));
+    let image   = Image::new();
     //A texture to use with the image
     let texture = Texture::from_path(Path::new("speed.png")).unwrap();
 
-    let draw_state = draw_state::DrawState::default();
+    let drawstate = draw_state::DrawState::default();
 
     //Main loop
     let mut events = window.events();
@@ -40,7 +46,7 @@ fn main() {
                 //Clear the screen
                 clear([0.0, 0.0, 0.0, 1.0], gl);
                 //Draw the image with the texture
-                image.draw(&image, &texture, &draw_state, c.transform, gl);
+                image.draw(&texture, &drawstate, c.transform, gl);
             });
         }
     }
